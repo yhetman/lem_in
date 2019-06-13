745f19810565708aef7c7fc035a2558ab25109dc
Yuliia HETMAN <yhetman@student.unit.ua>
2019-06-13 16:19:16 +0300

    parsing of rooms is MADE

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: yhetman <yhetman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 18:35:51 by yhetman           #+#    #+#             */
-/*   Updated: 2019/06/13 15:29:01 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/06/13 16:18:36 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,16 @@ void	in_case_of_error(t_stdin **temp, t_lem *lem, char *str)
 	ft_lstdel(temp, &ft_free_node);
 	free_t_lem(lem);
 	error_manager(str);
+}
+
+/*
+**			COUNTING PIPES
+*/
+
+
+bool		count_pipes(t_stdin **input, t_lem *lem)
+{
+	// to be continued....
 }
 
 
@@ -149,11 +159,60 @@ t_coord	get_coordinates(char *line)
 **			OTHERWISE THE ERROR'S OCURRED
 */
 
-
-
-bool	room_adding(t_lem **lem, char *line, int type)
+t_room	*init_room(char *name, int type, t_coord *c, int ants)
 {
-	t_room	*new;
+	t_room	*new_room;
+
+	new_room = ft_memalloc(sizeof(t_room));
+	new_room->c.x = c->x;
+	new_room->c.y = c->y;
+	new_room->name = name;
+	new_room->type = type;
+	if (type == 2)
+		new_room->capasity= ants;
+	else
+		new_room->capasity= 0;
+	new_room->next = NULL;
+	return (new_room);
+}
+
+bool	already_existing(t_room	*graph, char *name)
+{
+	while (graph)
+	{
+		if (!ft_strcmp (graph->name, name))
+			return (true);
+		graph = graph->next;
+	}
+	return (false);
+}
+
+bool	save_destination(t_lem *lem, char *name, int type)
+{
+	if (type == 2)
+	{
+		if (lem->start)
+		{
+			ft_memdel((void**)&name);
+			return (false);
+		}
+		lem->start = ft_strdup(name);
+	}
+	else if (type == 3)
+	{
+		if (lem->end)
+		{
+			ft_memdel((void**)&name);
+			return (false);
+		}
+		lem->end = ft_strdup(name);
+	}
+	return (true);
+}
+
+bool	parsing_rooms(t_lem **lem, char *line, int type)
+{
+	t_room	*new_room;
 	char	*name;
 	t_coord	c;
 
@@ -164,10 +223,29 @@ bool	room_adding(t_lem **lem, char *line, int type)
 		return (false);
 	}
 	c = get_coordinates(line);
-
+	if (type == 2 || type == 3)
+		if (!save_destination(*lem, name, type))
+			return (false);
+	if ((*lem)->rooms_list && already_existing((*lem)->rooms_list, name))
+	{
+		ft_strdel(&name);
+		return (false);
+	}
+	new_room = init_room(name, type, &c, (*lem)->ants);
+	new_room->next = (*lem)->rooms_list;
+	(*lem)->rooms_list = new_room;
+	return (true);
 }
 
-void	define_beggining(t_stdin **list, int *type, char **line)
+static inline bool	no_destination(t_lem *lem)
+{
+	if (!lem->start || !lem->end)
+		return (false);
+	else
+		return (lem->start && lem->end);
+}
+
+void	define_destination(t_stdin **list, int *type, char **line)
 {
 	if (!ft_strcmp(*line, "##strart"))
 		*type = 2;
@@ -201,9 +279,9 @@ void	count_rooms(t_stdin **list, t_lem *lem)
 				continue ;
 			}
 			else
-				define_beggining(list, &type, &line);
+				define_destination(list, &type, &line);
 		}
-		if (!room_adding(&lem, line, type))
+		if (!parsing_rooms(&lem, line, type))
 			return ;
 		(*list) = (*list)->next;
 	}
@@ -225,6 +303,11 @@ void	parsing(t_stdin **input, t_stdin **temp, t_lem *lem)
 		in_case_of_error(temp, lem, "ERROR: invalid amount of ants.");
 	*input = (*input)->next;
 	count_rooms(input, lem);
+	if (!lem->rooms_list || !no_destination(lem))
+		in_case_of_error(temp, lem, "ERROR: there's no beggining or end");
+	if (!count_pipes(input, lem))
+		in_case_of_error(temp, lem,
+		"ERROR: some troubles with pipes, please check everything twice.");		
 }
 
 
